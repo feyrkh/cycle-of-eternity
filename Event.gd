269a03335343c2e74fe19066edf504b2006fcc2f
Kevin <kevinhobbs@gmail.com>
2020-12-14 18:37:52 -0600

    More stuff

--- Event.gd
+++ Event.gd
@@ -7,3 +7,5 @@ signal organizer_entry_toggled # (organizer, organizer_entry_toggled, isTurnedOn
 
 signal set_mouse_image # (mouseImageNodeName)
 signal show_query_popup # (query_popup, query_popup_target)
+
+signal new_scene_loaded # (newScene)
